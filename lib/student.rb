4049f64e693cf0ff50d902710891d8b7bc836831
Wesley <wes.beck21@outlook.com>
2020-11-25 11:14:15 -0800

    commit tree 9f72b8ac569b44f948be3df13107ee23bcace29c

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+def initialize
+    @knowledge = []
+end
+
+def learn(string)
+    @knowledge.push("#{string}")
+end
+
+def knowledge
+    @knowledge
+end
 
 end
